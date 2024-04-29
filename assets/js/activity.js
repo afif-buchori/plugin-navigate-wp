bc1fbfaa0240b98b9ff94f64f22d945b0d556046
Krisna Adi <kmgkrisna23@gmail.com>
2024-04-29 10:52:34 +0800

    Update

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -22,22 +22,41 @@ if (packageOptActivity !== "") {
       });
     }
 
-    const ticketTypeAct = document.getElementById("ticket-type-act") || "";
-    if (ticketTypeAct !== "") {
-      const countType = ticketTypeAct.getAttribute("data-ticket");
-      for (let j = 0; j < parseInt(countType); j++) {
-        const btnDecrement =
-          document.getElementById(i + "qty-act-dec" + j) || "";
-        const btnIncrement =
-          document.getElementById(i + "qty-act-inc" + j) || "";
-        const qtyType = document.getElementById(i + "qty-type-act" + j) | "";
+    // const ticketTypeAct = document.getElementById("ticket-type-act") || "";
+    // if (ticketTypeAct !== "") {
+    //   const countType = ticketTypeAct.getAttribute("data-ticket");
+    //   for (let j = 0; j < parseInt(countType); j++) {
+    //     const btnDecrement =
+    //       document.getElementById(i + "qty-act-dec" + j) || "";
+    //     const btnIncrement =
+    //       document.getElementById(i + "qty-act-inc" + j) || "";
+    //     const qtyType = document.getElementById(i + "qty-type-act" + j) | "";
 
-        console.log(i + "qty-act-dec" + j);
-        // btnDecrement.addEventListener("click", function () {
-        //   console.log(qtyType);
-        //   // qtyType.innerHTML = parseInt(qtyType) - 1;
-        // });
-      }
-    }
+    //     console.log(i + "qty-act-dec" + j);
+    //     // btnDecrement.addEventListener("click", function () {
+    //     //   console.log(qtyType);
+    //     //   // qtyType.innerHTML = parseInt(qtyType) - 1;
+    //     // });
+    //   }
+    // }
   }
 }
+
+const newTicketTypeAct = document.querySelectorAll("#new-ticket-type-act");
+        newTicketTypeAct.forEach(function(element) {
+
+            var btnDecrement = element.getAttribute("data-qty-act-dec");
+            document.getElementById(btnDecrement).onclick = function() {
+                console.log("huhu " + btnDecrement);
+            }
+            
+            var btnIncrement = element.getAttribute("data-qty-act-inc");
+            document.getElementById(btnIncrement).onclick = function() {
+                console.log("huhu " + btnIncrement);
+            }
+            
+            var qtyType = element.getAttribute("data-qty-type-act");
+            document.getElementById(qtyType).onclick = function() {
+                console.log("huhu " + qtyType);
+            }
+        });
