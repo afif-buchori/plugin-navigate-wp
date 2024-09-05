d9f49244fd584b5ed911d364141ffc7ae358edf2
Krisna Adi <kmgkrisna23@gmail.com>
2024-09-05 11:42:45 +0800

    Update

--- tour_package/form-booking.php
+++ tour_package/form-booking.php
@@ -7,7 +7,8 @@ function enx_get_page_content($data)
     $url = API_TOUR_PACKAGE_URL . '/post/data-booking';
     $data->currency = str_replace("currency=", "", checkCurrency());
     $data_res = fetchPost($url, $data);
-    var_dump($data_res->countrys);
+    $rate = $data_res->service->rate;
+    $currency = $rate->currency->client_currency;
     ob_start();
 ?>
     <div class="enx-container site-wrapper">
@@ -43,35 +44,91 @@ function enx_get_page_content($data)
                         <div class="md:grid grid-cols-12 gap-x-16 pt-16 pb-5 xl:py-20">
                             <!-- SIDE BOOKING -->
                             <div class="col-span-4">
-                                <div class="mb-10 sticky top-10">
-                                    <div class="wiget shadow-lg rounded-xl mb-10 bg-white">
+                                <div class="mb-5">
+                                    <div class="wiget shadow-lg rounded-xl mb-5 bg-white">
                                         <h5 class="font-heading text-xl text-primary font-bold border-b-2 border-primary border-opacity-10 px-7 py-3">
                                             Tour Details
                                         </h5>
-                                        <!-- <php foreach ($data as $k_data => $data_booking) { >
-                                            <div class="py-5 px-7">
-                                                <p class="font-bold text-lg"><php echo $data_booking->activityName ></p>
-                                                <p class="font-bold" style="opacity: 0.7;">
-                                                    <php echo $data_booking->packageName >
-                                                </p>
-                                                <php foreach ($data_booking->ticketType as $type) { >
-                                                    <div class="flex justify-between pl-4 text-sm">
-                                                        <p><php echo $type->ticketName ></p>
-                                                        <p>x <php echo $type->ticketQty ></p>
-                                                    </div>
-                                                <php } >
-                                                <div class="w-full flex justify-between">
-                                                    <p>Total</p>
-                                                    <p><php echo $data_res->data->currency->symbol > <span
-                                                            class="font-bold"><php echo number_format($data_booking->total, $data_res->data->currency->digit) ></span>
-                                                    </p>
-                                                </div>
+                                        <div class="py-5 px-7">
+                                            <h6 class="font-bold"><?php echo $data_res->service->contents->title ?></h6>
+                                            <h6 class="mt-3"><?php
+                                                                $date = new DateTime($data_res->data_session->date);
+                                                                echo date_format($date, "l, d F Y") ?>
+                                            </h6>
+                                            <div class="flex justify-between mt-4">
+                                                <p class="pl-4">Adult x<?php echo $rate->adult->price_details->qty ?></p>
+                                                <p><?php echo $currency->symbol . " " . number_format($rate->adult->price, $currency->digit) ?></p>
                                             </div>
-                                        <php } > -->
+                                            <div class="flex justify-between">
+                                                <p class="pl-4">Child x<?php echo $rate->child->price_details->qty ?></p>
+                                                <p><?php echo $currency->symbol . " " . number_format($rate->child->price, $currency->digit) ?></p>
+                                            </div>
+                                            <div class="flex justify-between">
+                                                <p class="pl-4">Infant x<?php echo $rate->infant->price_details->qty ?></p>
+                                                <p><?php echo $currency->symbol . " " . number_format($rate->infant->price, $currency->digit) ?></p>
+                                            </div>
+                                        </div>
+                                        <div class="font-bold border-t border-primary border-opacity-10 px-7 py-3 flex justify-between">
+                                            <h6 class="font-bold">Total</h6>
+                                            <p><?php echo $currency->symbol . " " . number_format($rate->total->client_currency, $currency->digit) ?></p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="mb-5">
+                                    <div class="wiget shadow-lg rounded-xl mb-5 bg-white">
+                                        <h5 class="font-heading text-xl text-primary font-bold border-b-2 border-primary border-opacity-10 px-7 py-3">
+                                            Payment Settings
+                                        </h5>
+                                        <div class="py-5 px-7">
+                                            <?php foreach ($data_res->service->payment as $key => $payment) {
+                                                $payment_in_id = strtolower(str_replace(" ", "_", $payment->title)) . "_$key";
+                                            ?>
+                                                <div class="<?php echo $key > 0 ? "mt-4" : "" ?>">
+                                                    <input type="radio" name="payment-settings" id="<?php echo $payment_in_id ?>" value='<?php echo json_encode($payment) ?>'
+                                                        data-service-fee="<?php echo (float)$rate->service_fee ?>" data-currency='<?php echo json_encode($currency) ?>'>
+                                                    <label for="<?php echo $payment_in_id ?>" class="pl-4"><?php echo $payment->title ?></label>
+                                                    <?php foreach ($payment->detail as $detail) {
+                                                        $due_date = date_format(new DateTime($detail->dueDate), "D, d F Y H:i");
+                                                    ?>
+                                                        <div class="flex justify-between font-bold">
+                                                            <p><?php echo $detail->name ?></p>
+                                                            <p><?php echo $currency->symbol . " " . number_format($detail->amount, $currency->digit) ?></p>
+                                                        </div>
+                                                        <div class="flex justify-between">
+                                                            <p>Due Date</p>
+                                                            <p><?php echo $due_date ?></p>
+                                                        </div>
+                                                    <?php } ?>
+                                                </div>
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div class="wiget shadow-lg rounded-xl mb-5 bg-white">
+                                        <div class="flex justify-between px-7 py-3">
+                                            <h6 class="font-heading text-primary font-bold ">
+                                                Total
+                                            </h6>
+                                            <p id="total_booking"><?php echo $currency->symbol . " " . number_format(0, $currency->digit) ?></p>
+                                        </div>
+                                        <div class="flex justify-between px-7 py-3">
+                                            <h6 class="font-heading text-primary font-bold">
+                                                Service Fee
+                                            </h6>
+                                            <p id="service_fee_booking"><?php echo $currency->symbol . " " . number_format(0, $currency->digit) ?></p>
+                                        </div>
+                                        <div class="flex justify-between px-7 py-3">
+                                            <h6 class="font-heading text-primary font-bold">
+                                                Total Pay
+                                            </h6>
+                                            <p id="total_pay_booking"><?php echo $currency->symbol . " " . number_format(0, $currency->digit) ?></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-
                             <!-- SIDE FORM BOOKING -->
 
                             <form id="form-booking-tourpackage" class="col-span-8">
@@ -115,9 +172,11 @@ function enx_get_page_content($data)
                                         <label class="text-primary font-semibold block mb-3" for="phone">Phone:</label>
                                         <div class="relative">
 
-                                            <!-- <label for="phone_code_select2" id="phone_code_label"
+                                            <label for="phone_code_select2" id="phone_code_label"
                                                 class="absolute inset-y-0 left-0 cursor-pointer"
-                                                style="padding: 8px; font-size: 14px"><php echo $old->phone_code  ('+' . $old->phone_code) : '-Code-' >-</label> -->
+                                                style="padding: 8px; font-size: 14px">
+                                                <?php echo $old->phone_code ? ('+' . $old->phone_code) : '-Code-' ?>
+                                            </label>
                                             <div class="absolute top-7 left-0 flex w-full" style="z-index: -10">
                                                 <select name="phone_code" id="phone_code_select2" class="">
                                                     <option selected value="">--Code Phone--</option>
