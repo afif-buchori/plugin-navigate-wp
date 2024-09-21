4f10d54afbcc17f6aed477b6f837cb6dbc92d226
Krisna Adi <kmgkrisna23@gmail.com>
2024-09-21 13:47:40 +0800

    Update navigate.php

--- navigate.php
+++ navigate.php
@@ -9,6 +9,139 @@ Author URI: https://navigate.id
 */
 
 // This code is executed when the plugin is activated.
+// defined('ABSPATH') || exit;
+
+// if (!class_exists('navigateIdUpdate')) {
+
+//   class navigateIdUpdate
+//   {
+//     public $plugin_slug;
+//     public $version;
+//     public $cache_key;
+//     public $cache_allowed;
+
+//     public function __construct()
+//     {
+//       // Inisialisasi plugin
+//       $this->plugin_slug = plugin_basename(__DIR__);
+//       $this->version = '0.6'; // Versi saat ini, versi baru ada di info.json
+//       $this->cache_key = 'navigate_update';
+//       $this->cache_allowed = false;
+
+//       // Tambah filter dan action
+//       add_filter('plugins_api', array($this, 'info'), 20, 3);
+//       add_filter('site_transient_update_plugins', array($this, 'update'));
+//       add_action('upgrader_process_complete', array($this, 'purge'), 10, 2);
+//     }
+
+//     // Fungsi untuk mengambil info.json dari URL
+//     public function request()
+//     {
+//       $remote = get_transient($this->cache_key);
+
+//       if (false === $remote || !$this->cache_allowed) {
+//         $remote = wp_remote_get(
+//           'https://raw.githubusercontent.com/afif-buchori/plugin-navigate-wp/refs/heads/master/info.json', // Ganti dengan URL info.json
+//           array(
+//             'timeout' => 10,
+//             'headers' => array('Accept' => 'application/json')
+//           )
+//         );
+
+//         if (
+//           is_wp_error($remote)
+//           || 200 !== wp_remote_retrieve_response_code($remote)
+//           || empty(wp_remote_retrieve_body($remote))
+//         ) {
+//           return false;
+//         }
+
+//         set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
+//       }
+
+//       $remote = json_decode(wp_remote_retrieve_body($remote));
+//       return $remote;
+//     }
+
+//     // Fungsi untuk menampilkan info plugin
+//     public function info($res, $action, $args)
+//     {
+//       if ('plugin_information' !== $action || $this->plugin_slug !== $args->slug) {
+//         return $res;
+//       }
+
+//       $remote = $this->request();
+//       if (!$remote) {
+//         return $res;
+//       }
+
+//       $res = new stdClass();
+//       $res->name = $remote->name;
+//       $res->slug = $remote->slug;
+//       $res->version = $remote->version;
+//       $res->tested = $remote->tested;
+//       $res->requires = $remote->requires;
+//       $res->author = $remote->author;
+//       $res->author_profile = $remote->author_profile;
+//       $res->download_link = $remote->download_url;
+//       $res->trunk = $remote->download_url;
+//       $res->requires_php = $remote->requires_php;
+//       $res->last_updated = $remote->last_updated;
+
+//       $res->sections = array(
+//         'description' => $remote->sections->description,
+//         'installation' => $remote->sections->installation,
+//         'changelog' => $remote->sections->changelog
+//       );
+
+//       if (!empty($remote->banners)) {
+//         $res->banners = array(
+//           'low' => $remote->banners->low,
+//           'high' => $remote->banners->high
+//         );
+//       }
+
+//       return $res;
+//     }
+
+//     // Fungsi untuk mendeteksi pembaruan
+//     public function update($transient)
+//     {
+//       if (empty($transient->checked)) {
+//         return $transient;
+//       }
+
+//       $remote = $this->request();
+//       if (
+//         $remote
+//         && version_compare($this->version, $remote->version, '<')
+//         && version_compare($remote->requires, get_bloginfo('version'), '<=')
+//         && version_compare($remote->requires_php, PHP_VERSION, '<=')
+//       ) {
+//         $res = new stdClass();
+//         $res->slug = $this->plugin_slug;
+//         $res->plugin = plugin_basename(__FILE__);
+//         $res->new_version = $remote->version;
+//         $res->tested = $remote->tested;
+//         $res->package = $remote->download_url;
+
+//         $transient->response[$res->plugin] = $res;
+//       }
+
+//       return $transient;
+//     }
+
+//     // Hapus cache setelah update
+//     public function purge($upgrader, $options)
+//     {
+//       if ('update' === $options['action'] && 'plugin' === $options['type']) {
+//         delete_transient($this->cache_key);
+//       }
+//     }
+//   }
+
+//   new navigateIdUpdate();
+// }
 
 require_once(dirname(__FILE__) . '/inc/define.php');
 
@@ -190,7 +323,7 @@ function enx_load_style_and_script()
 
 function myplugin_add_css_to_head()
 {
-  ?>
+?>
   <style type="text/css" media="screen">
     :root {
       --color-text-primary: 52, 78, 65 !important;
@@ -419,13 +552,13 @@ function myplugin_add_css_to_head()
       border: 2px solid rgb(var(--color-primary)) !important;
     }
   </style>
-  <?php
+<?php
 }
 add_action('wp_head', 'myplugin_add_css_to_head');
 
 function initialize_654_select2()
 {
-  ?>
+?>
   <script>
     // jQuery(document).ready(function($) {
     //   $('#phone_code_select2').select2();
@@ -500,7 +633,7 @@ function initialize_654_select2()
 
     // });
   </script>
-  <?php
+<?php
 }
 add_action('wp_footer', 'initialize_654_select2');
 
