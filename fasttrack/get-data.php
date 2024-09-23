68f20cc565bc6ea439e6b39f8db18c4f75e10b30
Krisna Adi <kmgkrisna23@gmail.com>
2024-09-23 14:09:51 +0800

    update

--- fasttrack/get-data.php
+++ fasttrack/get-data.php
@@ -3,8 +3,7 @@
 function enx_get_list_data()
 {
     $url = API_FASTTRACK_URL . CreateParams();
-    if ($_GET['code'])
-        $url .= "&code=" . strtoupper($_GET['code']);
+    if (isset($_GET['code'])) $url .= "&code=" . strtoupper($_GET['code']);
     return fetchGet($url);
 }
 
@@ -30,7 +29,7 @@ function enx_service_get_addon($id)
 function enx_create_list($items)
 {
     foreach ($items as $item) {
-        ?>
+?>
         <div data-x-data data-x-ref="losAngeles"
             data-x-intersect="anime({ targets: $refs.losAngeles, translateY: [100, 0], opacity: [0, 1], duration: 500 ,easing: 'easeOutQuad' })"
             style="flex: 1 1 0">
@@ -39,11 +38,12 @@ function enx_create_list($items)
                 style="min-height: 480px">
                 <div class="absolute bg-cover bg-center origin-top w-full rounded-2xl transition duration-500 transform translate-y-[-10px] group-hover:scale-110"
                     style="background-image: url(<?php echo
-                        isset($item->image_url) && file_get_contents($item->image_url) !== false ?
-                        $item->image_url
-                        :
-                        "https://d3837chlpocfug.cloudfront.net/28338fc3-5be8-4666-9b73-babecc70a467/build/assets/ph-notfound-d1e5c849.png" ?>); height: 80%">
+                                                    isset($item->image_url) && @getimagesize($item->image_url) ?
+                                                        $item->image_url :
+                                                        'https://d3837chlpocfug.cloudfront.net/28338fc3-5be8-4666-9b73-babecc70a467/build/assets/ph-notfound-d1e5c849.png';
+                                                    ?>); height: 80%">
                 </div>
+
                 <!-- <div class="bg-primary absolute origin-top-right right-5 top-5 rounded text-secondary uppercase px-5 py-1 font-numbers text-xs tracking-wider">
                     4 tours
                 </div> -->
@@ -76,17 +76,17 @@ function enx_create_list($items)
 
         <!-- <div data-x-data data-x-ref="losAngeles" data-x-intersect="anime({ targets: $refs.losAngeles, translateY: [100, 0], opacity: [0, 1], duration: 500 ,easing: 'easeOutQuad' })">
             <a href="<?php // echo "/" . AIRPORT_SERVICE_LINK . "/" . $item->slug 
-                    ?>" class="group flex-1 block relative bg-cover rounded-2xl xl:my-0 overflow-hidden w-full h-tour xl:w-[400px] 2xl:w-[300px] h-[490px]">
+                        ?>" class="group flex-1 block relative bg-cover rounded-2xl xl:my-0 overflow-hidden w-full h-tour xl:w-[400px] 2xl:w-[300px] h-[490px]">
                 <div class="absolute bg-cover bg-center origin-top w-full h-5/6 rounded-2xl transition duration-500 transform translate-y-[-10px] group-hover:scale-110" style="background-image: url(<?php // echo $item->image_url 
-                        ?>);">
+                                                                                                                                                                                                        ?>);">
                 </div>
-                <!-- <div class="bg-primary absolute origin-top-right right-5 top-5 rounded text-secondary uppercase px-5 py-1 font-numbers text-xs tracking-wider">
+                <!- <div class="bg-primary absolute origin-top-right right-5 top-5 rounded text-secondary uppercase px-5 py-1 font-numbers text-xs tracking-wider">
                     4 tours
                 </div> -->
         <!-- <div class="absolute bottom-10 w-full bg-secondary transition duration-500 group-hover:bg-primary py-8 px-5 rounded-2xl">
                     <h3 class="font-heading text-xl text-transform-unset font-light text-primary transition duration-500 group-hover:text-white">
                         <?php // echo $item->title 
-                                ?>
+                        ?>
                     </h3>
                     <div class="w-full border-t border-primary border-opacity-40 my-6 transition duration-500 group-hover:border-white/30"></div>
                     <div class="flex justify-between items-center">
@@ -97,14 +97,14 @@ function enx_create_list($items)
                             <span class="block text-sm font-numbers">From</span>
                             <span class="block text-lg font-numbers font-bold">
                                 <?php // echo $item->currency->symbol 
-                                        ?><?php // echo number_format($item->rate, $item->currency->digit) 
-                                                ?>
+                                ?><?php // echo number_format($item->rate, $item->currency->digit) 
+                                    ?>
                             </span>
                         </div>
                     </div>
                 </div>
             </a>
         </div> -->
-        <?php
+<?php
     }
 }
--- navigate.php
+++ navigate.php
@@ -3,145 +3,146 @@
 Plugin Name: Navigate ID
 Plugin URI: https://navigate.id/plugins
 Description: A plugin that fetches data from an API and displays it on a page.
-Version: 0.6
+Version: 0.5
 Author: navigate.id
 Author URI: https://navigate.id
 */
 
 // This code is executed when the plugin is activated.
-// defined('ABSPATH') || exit;
+defined('ABSPATH') || exit;
 
-// if (!class_exists('navigateIdUpdate')) {
+if (!class_exists('navigateIdUpdate')) {
 
-//   class navigateIdUpdate
-//   {
-//     public $plugin_slug;
-//     public $version;
-//     public $cache_key;
-//     public $cache_allowed;
+  class navigateIdUpdate
+  {
+    public $plugin_slug;
+    public $version;
+    public $cache_key;
+    public $cache_allowed;
 
-//     public function __construct()
-//     {
-//       // Inisialisasi plugin
-//       $this->plugin_slug = plugin_basename(__DIR__);
-//       $this->version = '0.6'; // Versi saat ini, versi baru ada di info.json
-//       $this->cache_key = 'navigate_update';
-//       $this->cache_allowed = false;
+    public function __construct()
+    {
+      // Inisialisasi plugin
+      $this->plugin_slug = plugin_basename(__DIR__);
+      // $this->plugin_slug = 'navigate-id';
+      $this->version = '0.5'; // Versi saat ini, versi baru ada di info.json
+      $this->cache_key = 'navigate_update';
+      $this->cache_allowed = false;
 
-//       // Tambah filter dan action
-//       add_filter('plugins_api', array($this, 'info'), 20, 3);
-//       add_filter('site_transient_update_plugins', array($this, 'update'));
-//       add_action('upgrader_process_complete', array($this, 'purge'), 10, 2);
-//     }
+      // Tambah filter dan action
+      add_filter('plugins_api', array($this, 'info'), 20, 3);
+      add_filter('site_transient_update_plugins', array($this, 'update'));
+      add_action('upgrader_process_complete', array($this, 'purge'), 10, 2);
+    }
 
-//     // Fungsi untuk mengambil info.json dari URL
-//     public function request()
-//     {
-//       $remote = get_transient($this->cache_key);
+    // Fungsi untuk mengambil info.json dari URL
+    public function request()
+    {
+      $remote = get_transient($this->cache_key);
 
-//       if (false === $remote || !$this->cache_allowed) {
-//         $remote = wp_remote_get(
-//           'https://tondest.oladimhospitality.com/wp-content/uploads/updater/info.json', // Ganti dengan URL info.json
-//           array(
-//             'timeout' => 10,
-//             'headers' => array('Accept' => 'application/json')
-//           )
-//         );
+      if (false === $remote || !$this->cache_allowed) {
+        $remote = wp_remote_get(
+          'https://tondest.oladimhospitality.com/wp-content/uploads/updater/info.json', // Ganti dengan URL info.json
+          array(
+            'timeout' => 10,
+            'headers' => array('Accept' => 'application/json')
+          )
+        );
 
-//         if (
-//           is_wp_error($remote)
-//           || 200 !== wp_remote_retrieve_response_code($remote)
-//           || empty(wp_remote_retrieve_body($remote))
-//         ) {
-//           return false;
-//         }
+        if (
+          is_wp_error($remote)
+          || 200 !== wp_remote_retrieve_response_code($remote)
+          || empty(wp_remote_retrieve_body($remote))
+        ) {
+          return false;
+        }
 
-//         set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
-//       }
+        set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
+      }
 
-//       $remote = json_decode(wp_remote_retrieve_body($remote));
-//       return $remote;
-//     }
+      $remote = json_decode(wp_remote_retrieve_body($remote));
+      return $remote;
+    }
 
-//     // Fungsi untuk menampilkan info plugin
-//     public function info($res, $action, $args)
-//     {
-//       if ('plugin_information' !== $action || $this->plugin_slug !== $args->slug) {
-//         return $res;
-//       }
+    // Fungsi untuk menampilkan info plugin
+    public function info($res, $action, $args)
+    {
+      if ('plugin_information' !== $action || $this->plugin_slug !== $args->slug) {
+        return $res;
+      }
 
-//       $remote = $this->request();
-//       if (!$remote) {
-//         return $res;
-//       }
+      $remote = $this->request();
+      if (!$remote) {
+        return $res;
+      }
 
-//       $res = new stdClass();
-//       $res->name = $remote->name;
-//       $res->slug = $remote->slug;
-//       $res->version = $remote->version;
-//       $res->tested = $remote->tested;
-//       $res->requires = $remote->requires;
-//       $res->author = $remote->author;
-//       $res->author_profile = $remote->author_profile;
-//       $res->download_link = $remote->download_url;
-//       $res->trunk = $remote->download_url;
-//       $res->requires_php = $remote->requires_php;
-//       $res->last_updated = $remote->last_updated;
+      $res = new stdClass();
+      $res->name = $remote->name;
+      $res->slug = $remote->slug;
+      $res->version = $remote->version;
+      $res->tested = $remote->tested;
+      $res->requires = $remote->requires;
+      $res->author = $remote->author;
+      $res->author_profile = $remote->author_profile;
+      $res->download_link = $remote->download_url;
+      $res->trunk = $remote->download_url;
+      $res->requires_php = $remote->requires_php;
+      $res->last_updated = $remote->last_updated;
 
-//       $res->sections = array(
-//         'description' => $remote->sections->description,
-//         'installation' => $remote->sections->installation,
-//         'changelog' => $remote->sections->changelog
-//       );
+      $res->sections = array(
+        'description' => $remote->sections->description,
+        'installation' => $remote->sections->installation,
+        'changelog' => $remote->sections->changelog
+      );
 
-//       if (!empty($remote->banners)) {
-//         $res->banners = array(
-//           'low' => $remote->banners->low,
-//           'high' => $remote->banners->high
-//         );
-//       }
+      if (!empty($remote->banners)) {
+        $res->banners = array(
+          'low' => $remote->banners->low,
+          'high' => $remote->banners->high
+        );
+      }
 
-//       return $res;
-//     }
+      return $res;
+    }
 
-//     // Fungsi untuk mendeteksi pembaruan
-//     public function update($transient)
-//     {
-//       if (empty($transient->checked)) {
-//         return $transient;
-//       }
+    // Fungsi untuk mendeteksi pembaruan
+    public function update($transient)
+    {
+      if (empty($transient->checked)) {
+        return $transient;
+      }
 
-//       $remote = $this->request();
-//       if (
-//         $remote
-//         && version_compare($this->version, $remote->version, '<')
-//         && version_compare($remote->requires, get_bloginfo('version'), '<=')
-//         && version_compare($remote->requires_php, PHP_VERSION, '<=')
-//       ) {
-//         $res = new stdClass();
-//         $res->slug = $this->plugin_slug;
-//         $res->plugin = plugin_basename(__FILE__);
-//         $res->new_version = $remote->version;
-//         $res->tested = $remote->tested;
-//         $res->package = $remote->download_url;
+      $remote = $this->request();
 
-//         $transient->response[$res->plugin] = $res;
-//       }
+      if (
+        $remote
+        && version_compare($this->version, $remote->version, '<')
+        && version_compare($remote->requires, get_bloginfo('version'), '<=')
+        && version_compare($remote->requires_php, PHP_VERSION, '<=')
+      ) {
+        $res = new stdClass();
+        $res->slug = $this->plugin_slug;
+        $res->plugin = plugin_basename(__FILE__);
+        $res->new_version = $remote->version;
+        $res->tested = $remote->tested;
+        $res->package = $remote->download_url;
+        $transient->response[$res->plugin] = $res;
+      }
 
-//       return $transient;
-//     }
+      return $transient;
+    }
 
-//     // Hapus cache setelah update
-//     public function purge($upgrader, $options)
-//     {
-//       if ('update' === $options['action'] && 'plugin' === $options['type']) {
-//         delete_transient($this->cache_key);
-//       }
-//     }
-//   }
+    // Hapus cache setelah update
+    public function purge($upgrader, $options)
+    {
+      if ('update' === $options['action'] && 'plugin' === $options['type']) {
+        delete_transient($this->cache_key);
+      }
+    }
+  }
 
-//   new navigateIdUpdate();
-// }
+  new navigateIdUpdate();
+}
 
 require_once(dirname(__FILE__) . '/inc/define.php');
 
